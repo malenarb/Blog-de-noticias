2ffd88f7dc5326b9365196aa9f3bde2e356af050
malenarb <malenaromerob@gmail.com>
2025-08-07 22:52:28 -0300

    cambios en los atributos del modelo de categorias

--- BlogNoticias/apps/categorias/models.py
+++ BlogNoticias/apps/categorias/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
+    nombre = models.CharField(max_length=30, unique=True)
+    desc = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='categorias')
 
     def __str__(self):
         return self.nombre
